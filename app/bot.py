d843693274931ea4553b2067019c1e03559bb026
posharnick <p.runich@bk.ru>
2021-03-04 20:23:35 +0300

    lib removed from gitignore

--- app/bot.py
+++ app/bot.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher
 """Первоначальная настройка бота"""
 
 # Объявляем бота, 1 аргумент - токен бота, его можно получить в @BotFather
-bot = Bot('TOKEN')
+bot = Bot('1214569621:AAFXk_D3ODB6e88vKYN3NlTClSqgjL9gCsU')
 # Выбираем хранилище для хранения состояний FSM https://mastergroosha.github.io/telegram-tutorial-2/fsm/
 storage = MemoryStorage()
 # Объявляем обработчик комманд, 1 аргумент экземпляр бота, 2 какое хранилише для FSM мы используем
