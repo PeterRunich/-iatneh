ea085ed32d505ed1875c576851d58beeff72ad97
1-Holopsicon-1 <mfnbrx@gmail.com>
2021-03-04 22:03:25 +0300

    change system of cancel

--- app/helpers/start_kb_helper.py
+++ app/helpers/start_kb_helper.py
@@ -3,5 +3,4 @@ from ..keyboards.start_keyboard import start_kb_builder
 
 async def start_kb(msg):
         kb = await start_kb_builder()
-
-        await msg.reply('.', reply_markup=kb)
+        await msg.answer('Возвращение в главное меню', reply_markup=kb)
